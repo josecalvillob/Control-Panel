26952ab9dc91792589a5df56da45644de85f26f7
Jose Calvillo <josecalvillob@icloud.com>
2019-06-07 18:41:26 -0400

    add client

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/layout/Dashboard";
+import AddClient from "./components/clients/AddClient";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Dashboard} />
+              <Route exact path="/client/add" component={AddClient} />
             </Switch>
           </div>
         </div>
---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+//import { compose } from "redux";
+// import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
+class AddClient extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    balance: ""
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const {
+      state,
+      props: { firestore, history }
+    } = this;
+
+    const newClient = {
+      ...state,
+      balance: state.balance === "" ? "0" : state.balance
+    };
+
+    firestore
+      .add({ collection: "clients" }, newClient)
+      .then(() => history.push("/"));
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link">
+              <i className="fas fa-arrow-circle-left"> Back To Dashboard</i>
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Add Client</div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="firstName"
+                  minLength="2"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.firstName}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="lastName"
+                  minLength="2"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.lastName}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.email}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="phone"
+                  minLength="10"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.phone}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="balance">Balance</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  name="balance"
+                  onChange={this.onChange}
+                  value={this.state.balance}
+                />
+              </div>
+              <input type="submit" className="btn btn-primary btn-block" />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
